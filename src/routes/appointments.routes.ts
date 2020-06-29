cf29859deefe587a65cacbf33288cfd95301b2c9
Matheus Albino <matheusalb3213@gmail.com>
2020-06-29 10:32:18 -0300

    Aula 3, Validando a data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,23 +1,48 @@
-import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { Router } from "express";
+import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns"; // yarn add date-fns
+// startOfHour -> pega date e coloca uma hora no 0
+// parseISO -> converte uma data em string para data em tipo date do JS
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+// para evitar o erro de "any", defino a estrutura do objeto com uma interface
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+// antes, sem interface: "const appointments = [];""
+const appointments: Appointment[] = [];
 
 // rota que terá informações repassadas
-appointmentsRouter.post('/', (request, response) => {
-    const { provider, date } = request.body;
+appointmentsRouter.post("/", (request, response) => {
+  const { provider, date } = request.body;
 
-    const appointment = {
-        id: uuid(),
-        provider,
-        date,
-    }
+  const parsedDate = startOfHour(parseISO(date));
 
-    appointments.push(appointment);
+  // verificação se já existe um agendamento nessa data
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
 
-    return response.json(appointment);
+  // se houver
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: "This appointment is already booked." });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate, // antes, tava dando erro: "date: parsedDate,"
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
